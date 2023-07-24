f87b14b52869570c9d8444eb62bbaa3160347a0d
Saravanan Nandhan <sarancruzer@gmail.com>
2023-07-24 23:26:06 +0530

    dev branch deployed

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World! Hello World! Hello World! Hello World!';
   }
+
+  getHelloDev(): string {
+    return 'Hello World! development branch';
+  }
 }
