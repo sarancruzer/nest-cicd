10af708dee4ec6a6ab8f00f79cb96ee581a87206
Saravanan Nandhan <sarancruzer@gmail.com>
2023-07-25 00:04:11 +0530

    main branch changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Hello World! Hello World! Hello World! Welcome to new workflow';
+    return 'Hello World! Hello World! Hello World! Hello World! Welcome to new workflow Main branch changes';
   }
 }
